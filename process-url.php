a3eddd3a3337648aba59b23df20a10ef6e37f9d2
Mas Abdi <hub.halloabdi@gmail.com>
2025-11-26 08:48:53 +0700

    Delete process-url.php

--- process-url.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: application/json');
-$data = json_decode(file_get_contents('php://input'), true);
-$url = $data['url'];
-
-// Logika untuk memproses URL dan mendapatkan informasi video/audio (misalnya menggunakan youtube-dl atau API TikTok)
-$filename = "video.mp4"; // Sesuaikan dengan nama file default atau yang diambil dari URL
-
-echo json_encode(['success' => true, 'filename' => $filename]);
